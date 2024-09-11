f6623574bf764d122b5e5cda076a45ca5846f8fb
addison <meyerad@student.saviochs.org>
2024-09-11 11:05:18 -0500

    Practice Class Created

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeClass.java
@@ -1,2 +1,16 @@
-package org.firstinspires.ftc.teamcode;public class PracticeClass {
+package org.firstinspires.ftc.teamcode;
+
+/**
+ *  ----!please read this!----
+ *  This is a practice class I have created for our team. I am going to
+ *  base it off BasicOpMode_Iterative in "java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java"
+ *  Please mess around with the external.samples. That will get us started.
+ *  I can help you out during robotics on Thursday, if your having trouble. Good luck :)
+ *  ----!please read this!----
+ */
+
+public class PracticeClass {
+    public static void main(String[]args) {
+
+    }
 }
