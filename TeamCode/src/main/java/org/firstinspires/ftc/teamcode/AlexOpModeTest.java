962e3f386063cba90f4763625fcdf0f33855a1d6
at <testwhopper9999@gmail.com>
2024-09-12 20:10:10 -0500

    mecanum support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexOpModeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexOpModeTest.java
@@ -5,33 +5,28 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
 @Disabled
 public class AlexOpModeTest extends OpMode
-{   // OpMode members
+{
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor ld1 = null;
-    private DcMotor ld2 = null;
-    private DcMotor rd1 = null;
-    private DcMotor rd2 = null;
+    private DcMotor fl, fr, bl, br;
 
     @Override
     public void init()
     {
         telemetry.addData("Status", "init starting...");
 
-        // Hardware variable init
-        ld1 = hardwareMap.get(DcMotor.class, "left_drive_1");
-        ld2 = hardwareMap.get(DcMotor.class, "left_drive_2");
-        rd1 = hardwareMap.get(DcMotor.class, "right_drive_1");
-        rd2 = hardwareMap.get(DcMotor.class, "right_drive_2");
+        fl = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        fr = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        bl = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        br = hardwareMap.get(DcMotor.class, "backRightDrive");
 
-        ld1.setDirection(DcMotor.Direction.REVERSE);
-        ld2.setDirection(DcMotor.Direction.FORWARD);
-        rd1.setDirection(DcMotor.Direction.REVERSE);
-        rd2.setDirection(DcMotor.Direction.FORWARD);
+        fl.setDirection(DcMotor.Direction.REVERSE);
+        fr.setDirection(DcMotor.Direction.FORWARD);
+        bl.setDirection(DcMotor.Direction.REVERSE);
+        br.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.addData("Status", "INIT DONE");
     }
@@ -43,16 +38,20 @@ public class AlexOpModeTest extends OpMode
     @Override
     public void loop()
     {
-        double dx, dy = -gamepad1.left_stick_x;
-        double ld1Pow, ld2Pow, rd1Pow, rd2Pow;
+        double flPow, frPow, blPow, brPow;
+        double x = gamepad1.left_stick_x;
+        double y = -gamepad1.left_stick_y;
+        double z = gamepad1.right_stick_x;
+        double max = Math.max(Math.abs(y)+Math.abs(x)+Math.abs(z),1);
 
-        // broke rn fix later
-//        ld1Pow = x + y + z;
-//        ld2Pow = -x + y - z;
-//        rd1Pow = -x + y + z;
-//        rd2Pow = x + y - z;
+        flPow = (x + y + z)/max;
+        frPow = (-x + y - z)/max;
+        blPow = (-x + y + z)/max;
+        brPow = (x + y - z)/max;
 
-        double drive = -gamepad1.left_stick_y;
-        double turn = gamepad1.right_stick_x;
+        fl.setPower(flPow);
+        fr.setPower(frPow);
+        bl.setPower(blPow);
+        br.setPower(brPow);
     }
 }
