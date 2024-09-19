f0877a84b82f3321aa0b8ec0901c827e2d5b05ba
addison <meyerad@student.saviochs.org>
2024-09-19 16:15:01 -0500

    forced push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AddisonPracticeClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AddisonPracticeClass.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.util.Range;
  *  This is a practice class I have created for our team. I am going to
  *  base it off BasicOpMode_Iterative in "java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java"
  *  Please mess around with the external.samples. That will get us started.
- *  I can help you out during robotics on Thursday, if your having trouble. Good luck :)
+ *  I can help you out during robotics on Thursday,  your having trouble. Good luck :)
  *  ----!please read this!----
  */
 
@@ -31,15 +31,15 @@ public class AddisonPracticeClass extends OpMode{
         //initial message out
         telemetry.addData("status", "just started bro");
         //define motors
-        lOne = hardwareMap.get(DcMotor.class, "left_drive_one");
-        lTwo = hardwareMap.get(DcMotor.class, "left_drive_two");
-        rOne = hardwareMap.get(DcMotor.class, "right_drive_one");
-        rTwo = hardwareMap.get(DcMotor.class, "right_drive_two");
+        lOne = hardwareMap.get(DcMotor.class, "l1");
+        lTwo = hardwareMap.get(DcMotor.class, "l2");
+        rOne = hardwareMap.get(DcMotor.class, "r1");
+        rTwo = hardwareMap.get(DcMotor.class, "r2");
         //define motor direction
-        lOne.setDirection(DcMotor.Direction.REVERSE);
+        lOne.setDirection(DcMotor.Direction.FORWARD);
         lTwo.setDirection(DcMotor.Direction.REVERSE);
         rOne.setDirection(DcMotor.Direction.FORWARD);
-        rTwo.setDirection(DcMotor.Direction.FORWARD);
+        rTwo.setDirection(DcMotor.Direction.REVERSE);
         //initialized message out
         telemetry.addData("status", "Initialized");
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicDrive1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicDrive1.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
+@TeleOp(name="Basic: minh", group="Linear OpMode")
 @Disabled
 public class HolonomicDrive1 extends LinearOpMode {
 
