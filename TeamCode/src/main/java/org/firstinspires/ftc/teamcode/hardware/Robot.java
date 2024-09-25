c1d876bdaee21ed81a38b80dad0ed7042fd85e0c
at <testwhopper9000@gmail.com>
2024-09-25 12:54:33 -0500

    fix robot class 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode.hardware;
-import org.firstinspires.ftc.teamcode.lib.Config;
+import static org.firstinspires.ftc.teamcode.lib.Config.*;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -9,10 +9,10 @@ public class Robot
 
     public Robot init(HardwareMap hmap)
     {
-        fl = hmap.get(DcMotor.class, Config.FL_WHEEL);
-        fr = hmap.get(DcMotor.class, Config.FR_WHEEL);
-        bl = hmap.get(DcMotor.class, Config.BL_WHEEL);
-        br = hmap.get(DcMotor.class, Config.BR_WHEEL);
+        fl = hmap.get(DcMotor.class, FL_WHEEL);
+        fr = hmap.get(DcMotor.class, FR_WHEEL);
+        bl = hmap.get(DcMotor.class, BL_WHEEL);
+        br = hmap.get(DcMotor.class, BR_WHEEL);
 
         fl.setDirection(DcMotor.Direction.FORWARD);
         fr.setDirection(DcMotor.Direction.REVERSE);
