81f3116a736a909ac04810699f710ed53c25c318
john meyers <meyersjo@student.saviochs.org>
2024-09-27 15:30:08 -0500

    new Hang arm class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Hang_arm.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.lib.Config;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+
+public class Hang_arm {
+    public static DcMotor motor;
+    public static Servo servo;
+
+    public static Hang_arm Init(HardwareMap hmap){
+        motor = hmap.get(DcMotor.class,"hang_motor");
+        servo = hmap.get(Servo.class,"hand_servo");
+
+        return null;
+    }
+    public Hang_arm lift_hook (double pos){
+        servo.setPosition(pos);
+        return null;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.hardware;
 import static org.firstinspires.ftc.teamcode.lib.Config.*;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.hardware.Hang_arm;
 
 public class Robot
 {
@@ -9,6 +12,7 @@ public class Robot
 
     public Robot init(HardwareMap hmap)
     {
+        Hang_arm.Init(hmap);
         fl = hmap.get(DcMotor.class, FL_WHEEL);
         fr = hmap.get(DcMotor.class, FR_WHEEL);
         bl = hmap.get(DcMotor.class, BL_WHEEL);
