23557e3dffd44f4d79f5fc7180daa52a940ed0f5
whopper9999 <testwhopper9000@gmail.com>
2024-10-01 23:28:03 -0500

    Update Config.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Config.java
@@ -5,4 +5,6 @@ public class Config {
     public static final String FR_WHEEL = "frontRight";
     public static final String BL_WHEEL = "backLeft";
     public static final String BR_WHEEL = "backRight";
+    public static final double DEFAULT_SPEED = 1;
+    public static final double MOD_SPEED = 0.5;
 }
