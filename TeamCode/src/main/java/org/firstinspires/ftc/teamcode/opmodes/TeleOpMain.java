da74541ac8ca8f1f6d0f69cb66fe706d7199c764
whopper9999 <testwhopper9000@gmail.com>
2024-10-01 23:38:12 -0500

    implement speed change stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMain.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.lib.Config.*;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -15,7 +16,10 @@ public class TeleOpMain extends OpMode
     @Override
     public void loop()
     {
-        double x = gamepad1.left_stick_x, y = -gamepad1.left_stick_y, z = gamepad1.right_stick_x;
+        double speedMod = gamepad1.a ? SLOW_SPEED : DEFAULT_SPEED;
+        double turnMod = gamepad1.a ? SLOW_TURN : DEFAULT_TURN;
+        
+        double x = gamepad1.left_stick_x * speedMod, y = -gamepad1.left_stick_y * speedMod, z = gamepad1.right_stick_x * turnMod;
         double max = Math.max(Math.abs(y)+Math.abs(x)+Math.abs(z),1);
         
         robot.fl.setPower((x + y + z)/max);
