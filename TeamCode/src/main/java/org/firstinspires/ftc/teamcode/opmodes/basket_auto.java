81a59b9f33437ebb656df416a1c74de527670759
jasono9 <john.p.meyers809@gmail.com>
2024-10-11 15:20:28 -0500

    finished basket auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/basket_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/basket_auto.java
@@ -18,11 +18,11 @@ public class basket_auto extends LinearOpMode {
     Trajectory traj1;
     Trajectory traj2;
     Trajectory traj3;
+    Trajectory traj4;
     Pose2d start = new Pose2d(0,0,Math.toRadians(180));
     Pose2d clip = new Pose2d(27,0,Math.toRadians(180));
     Pose2d pickup1 = new Pose2d(24,-24,Math.toRadians(-45));
     Pose2d drop = new Pose2d(5,-53,Math.toRadians(45));
-    Pose2d pickup2 = new Pose2d(24,-36,Math.toRadians(-45));
     Pose2d pickup3 = new Pose2d(36,-24,Math.toRadians(-90));
     Pose2d park = new Pose2d(36,24,Math.toRadians(270));
     protected void clip() throws InterruptedException{
@@ -51,6 +51,30 @@ public class basket_auto extends LinearOpMode {
         bot.Block_Macro(controller,cur_time,Robot.Block_macro_state.grab);
         drive.followTrajectory(traj3);
         bot.getBlockarm().toggle_claw();
+        pickup1.plus(new Pose2d(0,-12,0));
+    }
+    protected void pickup2(double cur_time){
+        bot.getIntake().toggle_lower();
+        traj2 = drive.trajectoryBuilder(traj1.end())
+                .lineToSplineHeading(pickup3)
+                .back(5)
+                .build();
+        traj3 = drive.trajectoryBuilder(traj3.end())
+                .lineToSplineHeading(drop)
+                .build();
+        drive.followTrajectory(traj2);
+        bot.getIntake().toggle_beatbar();
+        bot.Block_Macro(controller,cur_time,Robot.Block_macro_state.grab);
+        drive.followTrajectory(traj3);
+        bot.getBlockarm().toggle_claw();
+    }
+    protected void park(){
+        traj4 = drive.trajectoryBuilder(traj3.end())
+                .lineToSplineHeading(park)
+                .build();
+        bot.getIntake().toggle_lower();
+        bot.getBlockarm().set_grab(block_arm.Position.preclip);
+        drive.followTrajectory(traj4);
     }
     @Override
     public void runOpMode() throws InterruptedException {
@@ -58,5 +82,9 @@ public class basket_auto extends LinearOpMode {
         clip();
         wait(20);
         pickup(time);
+        wait(20);
+        pickup2(time);
+        wait(20);
+        park();
     }
 }
