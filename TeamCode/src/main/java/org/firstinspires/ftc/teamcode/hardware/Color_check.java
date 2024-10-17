70efce2880fe4c7913a10dbfd55feec3d2d41566
jasono9 <john.p.meyers809@gmail.com>
2024-10-17 17:33:18 -0500

    probably broke stuff trying to get SDK to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Color_check.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Color_check.java
@@ -3,15 +3,15 @@ package org.firstinspires.ftc.teamcode.hardware;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-class Color_check {
+public class Color_check {
     ColorSensor c_sensor;
     intake intake;
-    Color_check Init(ColorSensor temp_c_sensor, HardwareMap HardwareMap){
+    public Color_check Init(ColorSensor temp_c_sensor, HardwareMap HardwareMap){
         c_sensor = temp_c_sensor;
         intake = new intake().Init(HardwareMap); // creates paradox
         return this;
     }
-    color getcolor(){
+    public color getcolor(){
         int sensitivity = 30;
         color temp_color;
         if (c_sensor.red()>c_sensor.blue()+sensitivity) temp_color = color.red;
