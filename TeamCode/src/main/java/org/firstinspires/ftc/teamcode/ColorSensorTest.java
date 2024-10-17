e41c24da4443ee5e4b8cd8b05bd87d67c509c839
Minh <nguyenmi@student.saviochs.org>
2024-10-17 16:32:08 -0500

    first

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -4,26 +4,27 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 
 @TeleOp(name="ColorSensor Opmode", group="Iterative OpMode")
-public class ColorSensorTest extends LinearOpMode{
+public class ColorSensorTest extends OpMode{
+    public ColorSensor frontColorSensor;
 
-    public ModernRoboticsI2cColorSensor frontColorSensor = null;
 
     @Override
-    public void runOpMode() {
-        frontColorSensor = hardwareMap.get(ModernRoboticsI2cColorSensor.class,"cS");
-
-        waitForStart();
-
+    public void init() {
+        frontColorSensor = hardwareMap.get(ColorSensor.class,"cS");
         frontColorSensor.enableLed(true);
+    }
 
-        while (opModeIsActive()); {
-            telemetry.addData("Color Number", frontColorSensor.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER));
-            telemetry.update();
-
-        }
+    @Override
+    public void loop() {
+        telemetry.addData("Red", frontColorSensor.red());
+        telemetry.addData("Green", frontColorSensor.green());
+        telemetry.addData("Blue", frontColorSensor.blue());
+        telemetry.update();
     }
 
 }
