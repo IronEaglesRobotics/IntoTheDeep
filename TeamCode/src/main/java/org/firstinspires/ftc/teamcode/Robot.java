080d695982c6b452d45fcf62d0cef080a9090834
Minh <nguyenmi@student.saviochs.org>
2024-11-03 21:24:23 -0600

    first

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -12,19 +12,27 @@ public class Robot {
     Lift lift;
     Claw claw;
     Intake intake;
+    Tilt tilt;
+    Cjoint1 cjoint1;
+    Cjoint2 cjoint2;
+    Cjoint3 cjoint3;
 
     public Robot(HardwareMap hardwareMap) {
         this.drive = new Drive(hardwareMap);
         this.claw = new Claw(hardwareMap);
         this.intake = new Intake(hardwareMap);
+        this.tilt = new Tilt(hardwareMap);
         this.lift = new Lift(hardwareMap);
         this.extendo = new Extendo(hardwareMap);
     }
 
     public static class Extendo {
         private Servo extendo;
-
+        public void setPosition(double position) {
+            this.extendo.setPosition(position);
+        }
         public Extendo(HardwareMap hardwareMap) {
+
             extendo = hardwareMap.servo.get("e");
         }
     }
@@ -33,27 +41,88 @@ public class Robot {
         private DcMotor lift;
 
         public Lift(HardwareMap hardwareMap) {
-            lift = hardwareMap.dcMotor.get("l");
+            lift = hardwareMap.get(DcMotor.class, "l");
 
-            lift.setDirection(DcMotorSimple.Direction.FORWARD);
+            lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+        public void setTargetPosition(int i) {
+            this.lift.setTargetPosition(i);
+        }
+
+        public void setPower(double v) {
+            this.lift.setPower(v);
+        }
+
+        public void setMode(DcMotor.RunMode runMode) {
+            this.lift.setMode(runMode);
         }
     }
 
     public static class Claw {
         private Servo claw;
+        public void setPosition(double position) {
+            this.claw.setPosition(position);
+        }
 
         public Claw(HardwareMap hardwareMap) {
             claw = hardwareMap.servo.get("c");
         }
     }
 
+    public static class Cjoint1 {
+        private Servo joint1;
+        public void setPosition(double position){
+            this.joint1.setPosition(position);
+        }
+        public Cjoint1(HardwareMap hardwareMap) {
+            joint1 = hardwareMap.servo.get("j1");
+        }
+    }
+
+    public static class Cjoint2 {
+        private Servo joint2;
+        public void setPosition(double position){
+            this.joint2.setPosition(position);
+        }
+        public Cjoint2(HardwareMap hardwareMap){
+            joint2 = hardwareMap.servo.get("j2");
+        }
+    }
+
+    public static class Cjoint3 {
+        private Servo joint3;
+        public void setPosition(double position){
+            this.joint3.setPosition(position);
+        }
+        public Cjoint3(HardwareMap hardwareMap){
+            joint3 = hardwareMap.servo.get("j3");
+        }
+    }
+
     public static class Intake {
-        private DcMotor intake;
+        private DcMotor spin;
+        public void setSpin(double power) {
+            this.spin.setPower(power);
+        }
 
         public Intake(HardwareMap hardwareMap) {
-            intake = hardwareMap.dcMotor.get("i");
+            spin = hardwareMap.dcMotor.get("s");
 
-            intake.setDirection(DcMotor.Direction.FORWARD);
+            spin.setDirection(DcMotor.Direction.FORWARD);
+        }
+    }
+
+    public static class Tilt {
+        private Servo tilt1;
+        private Servo tilt2;
+        public void setPosition(double position) {
+            this.tilt1.setPosition(position);
+            this.tilt2.setPosition(position);
+        }
+        public Tilt(HardwareMap hardwareMap) {
+            tilt1 = hardwareMap.servo.get("t1");
+            tilt2 = hardwareMap.servo.get("t2");
         }
     }
 
@@ -77,22 +146,23 @@ public class Robot {
 
         public void handleInput(Gamepad gamepad1, Gamepad gamepad2) {
             //defines inputs
-            double y = -gamepad1.left_stick_y;
+            double lsy = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-            double denominator = Math.max(Math.abs(y)+ Math.abs(x) + Math.abs(rx), 1);
+            double denominator = Math.max(Math.abs(lsy)+ Math.abs(x) + Math.abs(rx), 1);
 
             //sets motor power based on input
-            double FLPower = (y + x + rx / denominator);
-            double BLPower = (y - x + rx / denominator);
-            double FRPower = (y - x - rx / denominator);
-            double BRPower = (y + x - rx / denominator);
+            double FLPower = (lsy + x + rx / denominator);
+            double BLPower = (lsy - x + rx / denominator);
+            double FRPower = (lsy - x - rx / denominator);
+            double BRPower = (lsy + x - rx / denominator);
 
             FL.setPower(FLPower);
             BL.setPower(BLPower);
             FR.setPower(FRPower);
             BR.setPower(BRPower);
+
+            }
         }
     }
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class TeleOp extends OpMode {
     Robot robot;
@@ -13,5 +14,45 @@ public class TeleOp extends OpMode {
     @Override
     public void loop() {
         robot.drive.handleInput(this.gamepad1, this.gamepad2);
+
+        if(gamepad1.y) {
+            robot.extendo.setPosition(1);
+            robot.tilt.setPosition(1);
+        }
+
+        if(gamepad1.a) {
+            robot.tilt.setPosition(0);
+
+            robot.extendo.setPosition(0);
+        }
+
+        if(gamepad1.left_trigger > 0) {
+            robot.intake.setSpin(1);
+        }
+
+        if(gamepad2.right_bumper){
+            robot.claw.setPosition(.1);
+        }
+
+        if(gamepad2.y) {
+            robot.lift.setTargetPosition(1);
+            robot.lift.setPower(0.5);
+            robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.cjoint1.setPosition(1);
+            robot.cjoint2.setPosition(0);
+            robot.cjoint3.setPosition(-0.5);
+        }
+
+        if(gamepad2.a){
+            robot.lift.setTargetPosition(0);
+            robot.lift.setPower(0.5);
+            robot.lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.cjoint1.setPosition(0);
+            robot.cjoint2.setPosition(-0.25);
+            robot.cjoint3.setPosition(0);
+        }
     }
+
+
 }
