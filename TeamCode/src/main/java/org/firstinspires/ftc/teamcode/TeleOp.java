3a326e34ab0e65e8c552c3089e7292c776e451b2
Minh <nguyenmi@student.saviochs.org>
2024-11-09 09:55:23 -0600

    first

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -23,8 +23,8 @@ public class TeleOp extends OpMode {
     //auto (only after everything else)
 
 
-    public int intakeSteps = 0;
-    public int scoreSteps = 0;
+    public int intakeSteps = 1;
+    public int scoreSteps = 1;
 //    public double intakeTimer;
 
 
@@ -41,10 +41,26 @@ public class TeleOp extends OpMode {
     public static double TILTDOWN = .395;
 
 
+    public static int SLIDES_DOWN = 0;
+    public static int SLIDES_PICKUP = 100;
+    public static int SLIDES_SCORE1 = 620;
+    public static int SLIDES_SCORE2 = 400;
+
+    public static double armScore = .42;
+    public static double armRest = 0.5;
+    public static double elbowScore = 0.5;
+    public static double elbowRest = 0;
+    public static double wristScore = .75;
+    public static double wristRest = .59;
+    public static double clawOpen = .3;
+    public static double clawClose = 0;
+
+
+
     @Override
     public void init() {
         this.robot = new Robot(this.hardwareMap);
-        controller1=new GamepadEx(gamepad2);
+        controller1=new GamepadEx(gamepad1);
 
 //        robot.extendo.moveTo(0.26);
 //        robot.tiltRight.setPosition(0.1);
@@ -60,14 +76,15 @@ public class TeleOp extends OpMode {
 
 
     @Override
-
-
     public void loop() {
+
         controller1.readButtons();
-
         robot.drive.handleInput(gamepad1);
-
         intakeMacro(controller1);
+        scoreMacro(controller1);
+        telemetry.addData("scoreStep",scoreSteps);
+        telemetry.addData("intakStep", intakeSteps);
+        telemetry.update();
 
 
 //        if (gamepad1.y) {
@@ -99,85 +116,85 @@ public class TeleOp extends OpMode {
 //
 //        }
 
-        if(gamepad1.dpad_up){
-            //extend arm
-            robot.arm.setPosition(0.9);
-            robot.claw.setPosition(0);
-
-            //lift slides
-            robot.lift.setTargetPosition(600);
-            robot.lift.setMode(RUN_TO_POSITION);
-            robot.lift.setPower(0.25);
-        }
-
-        if(gamepad1.dpad_down){
-            //lowers slides?
-            robot.arm.setPosition(0.9);
-
-            robot.lift.setTargetPosition(250);
-            robot.lift.setMode(RUN_TO_POSITION);
-            robot.lift.setPower(0.75);
-            // robot.claw.setPosition(.2);
-
-            // robot.lift.setTargetPosition(100);
-            // robot.lift.setMode(RUN_TO_POSITION);
-            //  robot.lift.setPower(0.25);
-
-        }
-        if(gamepad1.dpad_right){
-            //
-            robot.arm.setPosition(0.9);
-            robot.elbow.setPosition(0);
-            robot.claw.setPosition(0);
-
-
-
-
-        }
-        if(gamepad1.dpad_left){
-            robot.claw.setPosition(0.2);
-            robot.arm.setPosition(1);
-
-            robot.lift.setTargetPosition(0);
-            robot.lift.setMode(RUN_TO_POSITION);
-            robot.lift.setPower(0.1);
-
-
-
-        }
-        if (controller1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER) && STATE) {
-            robot.claw.setPosition(.2);
-            STATE = false;
-        } else if (controller1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
-            robot.claw.setPosition(0);
-            STATE = true;
-        }
-        if(gamepad2.y){
-            robot.lift.setTargetPosition(300);
-            robot.lift.setMode(RUN_TO_POSITION);
-            robot.lift.setPower(-.25);
-        }
-        if(gamepad2.a){
-            robot.lift.setTargetPosition(0);
-            robot.lift.setMode(RUN_TO_POSITION);
-            robot.lift.setPower(.1);
-        }
-        if(gamepad2.b) {
-            robot.arm.setPosition(arm);
-            robot.elbow.setPosition(elbow);
-            robot.wrist.setPosition(0);
-        }
-        if(gamepad2.x){
-            robot.claw.setPosition(0);
-            robot.arm.setPosition(0);
-            robot.elbow.setPosition(0);
-            robot.wrist.setPosition(0);
-        }
+//        if(gamepad1.dpad_up){
+//            //extend arm
+//            robot.arm.setPosition(0.9);
+//            robot.claw.setPosition(0);
+//
+//            //lift slides
+//            robot.lift.setTargetPosition(600);
+//            robot.lift.setMode(RUN_TO_POSITION);
+//            robot.lift.setPower(0.25);
+//        }
+//
+//        if(gamepad1.dpad_down){
+//            //lowers slides?
+//            robot.arm.setPosition(0.9);
+//
+//            robot.lift.setTargetPosition(250);
+//            robot.lift.setMode(RUN_TO_POSITION);
+//            robot.lift.setPower(0.75);
+//            // robot.claw.setPosition(.2);
+//
+//            // robot.lift.setTargetPosition(100);
+//            // robot.lift.setMode(RUN_TO_POSITION);
+//            //  robot.lift.setPower(0.25);
+//
+//        }
+//        if(gamepad1.dpad_right){
+//            //
+//            robot.arm.setPosition(0.9);
+//            robot.elbow.setPosition(0);
+//            robot.claw.setPosition(0);
+//
+//
+//
+//
+//        }
+//        if(gamepad1.dpad_left){
+//            robot.claw.setPosition(0.2);
+//            robot.arm.setPosition(1);
+//
+//            robot.lift.setTargetPosition(0);
+//            robot.lift.setMode(RUN_TO_POSITION);
+//            robot.lift.setPower(0.1);
+//
+//
+//
+//        }
+//        if (controller1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER) && STATE) {
+//            robot.claw.setPosition(.2);
+//            STATE = false;
+//        } else if (controller1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
+//            robot.claw.setPosition(0);
+//            STATE = true;
+//        }
+//        if(gamepad2.y){
+//            robot.lift.setTargetPosition(300);
+//            robot.lift.setMode(RUN_TO_POSITION);
+//            robot.lift.setPower(-.25);
+//        }
+//        if(gamepad2.a){
+//            robot.lift.setTargetPosition(0);
+//            robot.lift.setMode(RUN_TO_POSITION);
+//            robot.lift.setPower(.1);
+//        }
+//        if(gamepad2.b) {
+//            robot.arm.setPosition(arm);
+//            robot.elbow.setPosition(elbow);
+//            robot.wrist.setPosition(0);
+//        }
+//        if(gamepad2.x){
+//            robot.claw.setPosition(0);
+//            robot.arm.setPosition(0);
+//            robot.elbow.setPosition(0);
+//            robot.wrist.setPosition(0);
+//        }
 
 
     }
 
-    public void intakeMacro(GamepadEx controller1) {
+    public void intakeMacro(GamepadEx cont1) {
         switch (intakeSteps){
             case 1: //Idle state
                 //Actions
@@ -186,20 +203,18 @@ public class TeleOp extends OpMode {
                 robot.extendo.moveTo(EXTENDO_RETRACT);
 
                 //switch to extended state
-                if(controller1.wasJustPressed(GamepadKeys.Button.Y)){ //BUTTON
+                if(cont1.wasJustPressed(GamepadKeys.Button.Y)){ //BUTTON
                     intakeSteps ++;
-                } else if(controller1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.3){ //BUTTON
-                    robot.intake.setSpin(1);
-                } else {
-                    robot.intake.setSpin(0);
-                }
+                } else if(cont1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.3){ //BUTTON
+                    robot.intake.setSpin(-1);
+                } else {robot.intake.setSpin(0);}
                 break;
             case 2: // Extended state
                 //Actions
                 robot.extendo.moveTo(EXTENDO_EXTEND);
 
                 //Switch to intaking state
-                if(controller1.wasJustPressed(GamepadKeys.Button.Y)){ //BUTTON
+                if(cont1.wasJustPressed(GamepadKeys.Button.Y)){ //BUTTON
                     intakeSteps ++;
                 }
                 break;
@@ -208,14 +223,14 @@ public class TeleOp extends OpMode {
                 robot.tiltRight.setPosition(TILTDOWN); //0.5
                 robot.tiltLeft.setPosition(TILTDOWN);
 
-                if(controller1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.3){ //BUTTON
-                    robot.intake.setSpin(1);
-                } else {
+                if(cont1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.3){ //BUTTON
                     robot.intake.setSpin(-1);
+                } else {
+                    robot.intake.setSpin(1);
                 }
 
-                if(controller1.wasJustPressed(GamepadKeys.Button.Y)){ //BUTTON
-                    intakeSteps = 0;
+                if(cont1.wasJustPressed(GamepadKeys.Button.Y)){ //BUTTON
+                    intakeSteps = 1;
                 }
                 break;
         }
@@ -227,19 +242,56 @@ public class TeleOp extends OpMode {
             case 1: //Idle state
                 //Actions
 
+                robot.arm.setPosition(armRest);
+                robot.lift.setTargetPosition(SLIDES_DOWN, .1);
+                robot.wrist.setPosition(wristRest);
+                robot.elbow.setPosition(elbow);
+                robot.claw.setPosition(clawOpen);
+
                 //put retracted normal positions here
+                if(controller1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+                    scoreSteps++;
+                }
 
                 break;
             case 2: // Extended state
                 //Actions
-
+                robot.lift.setTargetPosition(SLIDES_PICKUP,1);
                 //
-
+                if(controller1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+                    scoreSteps++;
+                }
                 break;
             case 3: //Intaking
                 //actions
-
+                robot.arm.setPosition(armScore);
+                robot.elbow.setPosition(elbowScore);
+                if(controller1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+                    scoreSteps++;
+                }
                 break;
+            case 4: //Intaking
+                //actions
+                robot.claw.setPosition(clawClose);
+                if(controller1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+                    scoreSteps++;
+                }
+                break;
+            case 5: //Intaking
+                //actions
+                robot.lift.setTargetPosition(SLIDES_SCORE1,1);
+                if(controller1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+                    scoreSteps++;
+                }
+                break;
+            case 6: //Intaking
+                //actions
+                robot.lift.setTargetPosition(SLIDES_SCORE2,1);
+                if(controller1.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+                    scoreSteps = 1;
+                }
+                break;
+
         }
 
     }
