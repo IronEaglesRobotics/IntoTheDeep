d43f800e20ef257059231a1abcac62784f2b954d
addison <meyerad@student.saviochs.org>
2024-11-18 16:16:45 -0600

    reverted to old roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest.java
@@ -1,5 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-public class RoadRunnerTest {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.drive.MecanumDrive;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.acmerobotics.roadrunner.util.NanoClock;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.control.PIDFController;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+public class RoadRunnerTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
